6720a160e62052221abab664a7b7e95b49ae9302
Stuart Reid <stuart1334@gmail.com>
2021-05-18 17:36:03 +0100

    Update test

--- test/AlgorithmsDotNet.Tests/DataStructures/Lists/DoublyLinkedListTests.cs
+++ test/AlgorithmsDotNet.Tests/DataStructures/Lists/DoublyLinkedListTests.cs
@@ -1,23 +1,32 @@
 ﻿using AlgorithmsDotNet.DataStructures.Lists;
+using AlgorithmsDotNet.Tests.FsCheck;
 using FsCheck.Xunit;
+using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace AlgorithmsDotNet.Tests.DataStructures.Lists
 {
     public class DoublyLinkedListTests
     {
-        [Property]
-        public void Add_ToEmptyList_IncrementsCount(int itemToAdd)
+        [Property(Arbitrary = new Type[] { typeof(IntLists) })]
+        public void Add_ToList_IncrementsCountAndAddsItem(List<int> data)
         {
             // Arrange
             var list = new DoublyLinkedList<int>();
 
             // Act
-            list.Add(itemToAdd);
+            var i = 0;
+            foreach(var item in data)
+            {
+                // Act
+                list.Add(item);
+                i++;
 
-            // Assert
-            Assert.Single(list);
-            Assert.Equal(itemToAdd, list[0]);
+                // Assert
+                Assert.Equal(i, list.Count);
+                Assert.Equal(item, list[i - 1]);
+            }
         }
     }
 }
